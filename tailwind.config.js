d06d233bc66623638d30976ba5a335e9c994f597
Awifu <spendawifu@gmail.com>
2025-08-10 01:50:15 +0300

    Refactor server.js, add migrate-dryrun, update package.json, setup Tailwind build

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./public/**/*.html",
+    "./public/js/**/*.js"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: "#f5f7ff",
+          100: "#eaeefe",
+          200: "#cfd9fd",
+          300: "#a7bbfb",
+          400: "#7994f7",
+          500: "#5b78f2",
+          600: "#4159e6",
+          700: "#3246c2",
+          800: "#2b3a99",
+          900: "#253277"
+        }
+      }
+    }
+  },
+  plugins: [],
+}
