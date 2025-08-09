b18fce923d053bf729841e81757bd076c9f069b4
Awifu <spendawifu@gmail.com>
2025-08-09 23:22:05 +0300

    fix: clean server.js to remove invalid characters

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-﻿@'
-const express = require("express");
+﻿const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/healthz", (req, res) => res.send("ok"));
-app.get("/", (req, res) => res.send("TidyZenic SaaS (Node) is running."));
+app.get("/healthz", (req, res) => {
+  res.send("ok");
+});
+
+app.get("/", (req, res) => {
+  res.send("TidyZenic SaaS (Node) is running.");
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-'@ | Out-File -Encoding utf8 server.js
